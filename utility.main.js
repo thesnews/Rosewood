4e3d3b5a7bb5085a78a1dd9481c55b4847c64fcc
Mike Joseph <mike.joseph5@gmail.com>
2010-03-22 12:29:44 -0400

    fixed typo in path name

--- utility.main.js
+++ utility.main.js
@@ -14,7 +14,7 @@ if( !window['Rose'] ) {
 	 */
 	var base = window.location.toString();
 	if( base.indexOf( 'index.php' ) != -1 ) {
-		base = ROSE_BASE_PATH.substr( 0,	base.indexOf( 'index.php' ) );
+		base = base.substr( 0,	base.indexOf( 'index.php' ) );
 	}
 	
 	Rose.basePath = base;
