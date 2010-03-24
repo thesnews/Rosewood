a4948815bc402d2307f5128346e043be0016ec09
Mike Joseph <mike.joseph5@gmail.com>
2010-03-24 16:39:47 -0400

    starting on button, added workables and sprites

---
+++ ui/ui.button.js
@@ -0,0 +1,24 @@
+if( !window['Rose'] ) {
+	Rose = {};
+}
+(function() {
+
+	var Button = new Class({
+
+	});
+	
+	var ButtonBar = new Class({
+	
+	});
+	
+	Element.implement({
+		buttonify: function() {
+		
+		},
+		
+		buttonBarify: function() {
+		
+		}
+	});
+	
+})();
